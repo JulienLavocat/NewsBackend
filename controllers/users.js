369bc13a7cd285e950cad146a02641b7d0fa9450
Aayyi <julien.lavocat@gmail.com>
2019-06-04 12:56:57 +0200

    Last commit for now

---
+++ controllers/users.js
@@ -0,0 +1,10 @@
+async function register(req, res) {
+
+}
+
+async function login(req, res) {
+
+}
+
+exports.register = register;
+exports.login = login;
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
-router.use("/", require("./app"));
+router.use("/", require("./service"));
+router.use("/users/", require("./users"));
 
 module.exports = router;
